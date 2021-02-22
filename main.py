109d35cd671739acc3bf49b60167794c3b3b5a97
joshuacook <joshuacook>
2021-02-22 20:53:57 +0000

    add strings file

--- main.py
+++ main.py
@@ -7,4 +7,28 @@ my_package.math.squared
 
 # COMMAND ----------
 
+import my_package.math
+
+# COMMAND ----------
+
+from my_package.math import squared
+
+# COMMAND ----------
+
+# MAGIC %run ./my_package/math 
+
+# COMMAND ----------
+
+squared(4)
+
+# COMMAND ----------
+
+from my_package import strings
+
+# COMMAND ----------
+
+strings.concatenate("foo", "bar")
+
+# COMMAND ----------
+
 
